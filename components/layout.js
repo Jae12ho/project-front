c0626491d66b13625cffecde4f6ea68a4b69cbe2
Jaeho Yang <ho4121e@gmail.com>
2022-10-08 11:56:39 +0900

    Docs: components 폴더 생성 && Feat: 레이아웃 컴포넌트 추가

---
+++ components/layout.js
@@ -0,0 +1,12 @@
+// import Header from './header'
+// import Footer from './footer'
+
+export default function Layout({ children }) {
+    return (
+        <div className="layout">
+            {/* <Header /> */}
+            <div>{children}</div>
+            {/* <Footer /> */}
+        </div>
+    )
+}
